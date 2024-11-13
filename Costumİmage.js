2bea77f5f8aabcae0db3db7fc82208bd0137e980
KJUW <12sametaltun12@gmail.com>
2024-11-13 16:28:10 +0300

    Update Costumİmage.js

--- Costumİmage.js
+++ Costumİmage.js
@@ -1,7 +1,9 @@
-const divElement = document.querySelector('.dzdZB1_85a5034ad3ee4f2');
-if (divElement) {
-  divElement.innerHTML = `
-    <img src="https://cdn.discordapp.com/avatar-decoration-presets/a_d1ea7b8650bf3d64a03304c2ceb7d089.png" alt="" class="hcY2u1_618fdd889c0717e" style="top:-7%">
-    <img src="https://r2.guns.lol/24ff5b0c-876f-4cda-af59-a6cb573bbb30.png" alt="" class="n2etgY_229bedc9b07046b false">
-  `;
-}
+setInterval(() => {
+  const divElement = document.querySelector('.dzdZB1_85a5034ad3ee4f2');
+  if (divElement) {
+    divElement.innerHTML = `
+      <img src="https://cdn.discordapp.com/avatar-decoration-presets/a_d1ea7b8650bf3d64a03304c2ceb7d089.png" alt="" class="hcY2u1_618fdd889c0717e" style="top:-7%">
+      <img src="https://r2.guns.lol/24ff5b0c-876f-4cda-af59-a6cb573bbb30.png" alt="" class="n2etgY_229bedc9b07046b false">
+    `;
+  }
+}, 0); // Her 1000 milisaniyede (1 saniyede) bir çalışır
