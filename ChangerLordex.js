2a36d992a352649e62b9706f36a0d276ff265f20
KJUW <12sametaltun12@gmail.com>
2024-11-13 15:21:09 +0300

    Create ChangerLordex.js

---
+++ ChangerLordex.js
@@ -0,0 +1,6 @@
+setInterval(() => {
+  const element = document.querySelector('h1.lOj1YV_f8ef53b912061d9');
+  if (element && element.innerText.includes("lordex.lol")) {
+    element.innerText = element.innerText.replace("lordex.lol", "LORDEX");
+  }
+}, 0);
