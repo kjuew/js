e95d9d28599fba47dc32b39232f54072d3179b3c
KJUW <12sametaltun12@gmail.com>
2024-11-13 17:22:05 +0300

    Update VolumeControlDeleter.js

--- VolumeControlDeleter.js
+++ VolumeControlDeleter.js
@@ -1,5 +1,7 @@
-const sliderElement = document.querySelector('.jWbbZU_8629220724424aa');
-
-if (sliderElement) {
-    sliderElement.remove();
-}
+setInterval(function() {
+    const sliderElement = document.querySelector('.jWbbZU_8629220724424aa');
+    
+    if (sliderElement) {
+        sliderElement.remove();
+    }
+}, 0);
