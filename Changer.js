45b6c8d01caaba56532def2c4a352de8663298b0
KJUW <12sametaltun12@gmail.com>
2024-11-13 17:05:34 +0300

    Update Changer.js

--- Changer.js
+++ Changer.js
@@ -1,9 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
+setInterval(() => {
   const element = document.querySelector('.p3WVzB_599d5ef2ae28184 span');
-  if (element) {
-    const textNode = element.lastChild;
-    if (textNode && textNode.nodeType === 3) {
-      textNode.textContent = "1,987";
-    }
+  if (element && element.textContent.includes("0")) {
+    element.textContent = "1,965";  // Sayıyı 1,965 ile değiştir
   }
-});
+}, 100); // Her 100 milisaniyede bir çalışır
