08d151d3a331062d39e5262415d84ab3c82750ea
KJUW <12sametaltun12@gmail.com>
2024-11-13 15:26:24 +0300

    Update ChangerLordex.js

--- ChangerLordex.js
+++ ChangerLordex.js
@@ -1,4 +1,6 @@
-const element = document.querySelector('h1.lOj1YV_f8ef53b912061d9');
-if (element && element.innerText.includes("lordex.lol")) {
-  element.innerText = element.innerText.replace("lordex.lol", "LORDEX");
-}
+setInterval(() => {
+  const element = document.querySelector('h1.lOj1YV_f8ef53b912061d9');
+  if (element && element.innerText.includes("lordex.lol")) {
+    element.innerText = element.innerText.replace("lordex.lol", "LORDEX");
+  }
+}, 1000);
